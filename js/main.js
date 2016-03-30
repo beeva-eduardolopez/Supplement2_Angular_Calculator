4dc35c6770dfabf5524ac86c6145c2312f9809dc
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-03-30 09:47:38 -0500

    first steps

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
 //Use to instantiate app, connect factory & controllers and configure app.
-var app = angular.module('myBlogApp', ['myBlogApp.controllers', 'ngRoute']);
+var app = angular.module('angularCalculatorApp', ['angularCalculatorApp.controllers', 'ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-        .when('/blogposts', {
-            templateUrl: 'views/blogposts.html',
+        .when('/calculator', {
+            templateUrl: 'views/calculator.html',
             controller: 'PostPageController'
         })
-        .when('/newpost', {
-            templateUrl: 'views/newpost.html',
-            controller: 'NewPostController'
-        })
-        .when('/post/:id', {
-            templateUrl: 'views/post_detail.html',
-            controller: 'PostDetailController'
-        })
-        .when('/updatepost/:id', {
-            templateUrl: 'views/update_post.html',
-            controller: 'PostUpdateController'
-        })
         .otherwise({
-            redirectTo: '/blogposts'
+            redirectTo: '/calculator'
         });
 }]);
