35f0c4416d315a44901e7a1046df83461d7e94c3
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-03-30 17:24:46 -0500

    add p with the info

--- main.js
+++ main.js
@@ -1,36 +1,45 @@
 angular.module('angularCalculatorApp', [])
     .controller('MainController', function($scope) {
+        $scope.hidden = 'hidden';
         $scope.onClickSum = function() {
             $scope.result = calculateSum();
             function calculateSum() {
                 return $scope.op1 + $scope.op2;
             }
-            $scope.op1 = '';
-            $scope.op2 = '';
+            $scope.operation = 'add';
+            $scope.hidden = 'visible';
+            // $scope.op1 = '';
+            // $scope.op2 = '';
         };
         $scope.onClickSubtract = function() {
             $scope.result = calculateSub();
             function calculateSub() {
                 return $scope.op1 - $scope.op2;
             }
-            $scope.op1 = '';
-            $scope.op2 = '';
+            $scope.operation = 'subtract';
+            $scope.hidden = 'visible';
+            // $scope.op1 = '';
+            // $scope.op2 = '';
         };
         $scope.onClickMultiply = function() {
             $scope.result = calculateMul();
             function calculateMul() {
                 return $scope.op1 * $scope.op2;
             }
-            $scope.op1 = '';
-            $scope.op2 = '';
+            $scope.operation = 'multiply';
+            $scope.hidden = 'visible';
+            // $scope.op1 = '';
+            // $scope.op2 = '';
         };
         $scope.onClickDivide = function() {
             $scope.result = calculateDiv();
             function calculateDiv() {
                 return $scope.op1 / $scope.op2;
             }
-            $scope.op1 = '';
-            $scope.op2 = '';
+            $scope.operation = 'divide';
+            $scope.hidden = 'visible';
+            // $scope.op1 = '';
+            // $scope.op2 = '';
         };
 
     });
