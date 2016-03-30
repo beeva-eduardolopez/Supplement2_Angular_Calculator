fee9f49b7f32d5970c930139a60df2bdedf1ef59
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-03-30 13:48:55 -0500

    clean inputs

--- main.js
+++ main.js
@@ -5,24 +5,32 @@ angular.module('angularCalculatorApp', [])
             function calculateSum() {
                 return $scope.op1 + $scope.op2;
             }
+            $scope.op1 = '';
+            $scope.op2 = '';
         };
         $scope.onClickSubtract = function() {
             $scope.result = calculateSub();
             function calculateSub() {
                 return $scope.op1 - $scope.op2;
             }
+            $scope.op1 = '';
+            $scope.op2 = '';
         };
         $scope.onClickMultiply = function() {
             $scope.result = calculateMul();
             function calculateMul() {
                 return $scope.op1 * $scope.op2;
             }
+            $scope.op1 = '';
+            $scope.op2 = '';
         };
         $scope.onClickDivide = function() {
             $scope.result = calculateDiv();
             function calculateDiv() {
                 return $scope.op1 / $scope.op2;
             }
+            $scope.op1 = '';
+            $scope.op2 = '';
         };
 
     });
